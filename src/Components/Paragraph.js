6d99041930f12f2f13541c85f7d64274c728fd6c
kodderkarthik <kkrishnegowdaostb1@gmail.com>
2019-07-29 16:45:18 +0530

    commited

--- src/Components/Paragraph.js
+++ src/Components/Paragraph.js
@@ -1,132 +1,72 @@
-import React, { Component } from 'react';
-import '../Css/News.css';
-// import { Carousel } from "react-responsive-carousel";
-
-class Paragraph extends Component {
-    render() {
-        return (
-            <div>
-                {/* <Carousel autoPlay>
-                    <div className='row'>
-                        <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'></div>
-                        <div className='col-xs-8 col-sm-8 col-md-8 col-lg-8'>
-                            <div><h1>PARAGRAPH #1</h1></div>
-                            <p>During the Middle Ages, hospitals served different functions from modern institutions. Middle Ages 
-                                hospitals were almshouses for the poor, hostels for pilgrims, or hospital schools. 
-                                The word "hospital" comes from the Latin hospes, signifying a stranger or foreigner, hence a guest. 
-                                Another noun derived from this, hospitium came to signify hospitality, that is the relation between 
-                                guest and shelterer, hospitality, friendliness, and hospitable reception.</p>
-                        </div>
-                        <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'></div>  
-                    </div>
-                    <div className='row'>
-                        <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'></div>
-                        <div className='col-xs-8 col-sm-8 col-md-8 col-lg-8'>
-                            <div><h1>PARAGRAPH #2</h1></div>
-                            <p>During the Middle Ages, hospitals served different functions from modern institutions. Middle Ages 
-                                hospitals were almshouses for the poor, hostels for pilgrims, or hospital schools. 
-                                The word "hospital" comes from the Latin hospes, signifying a stranger or foreigner, hence a guest. 
-                                Another noun derived from this, hospitium came to signify hospitality, that is the relation between 
-                                guest and shelterer, hospitality, friendliness, and hospitable reception.</p>
-                        </div>
-                        <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'></div>  
-                    </div>
-                    <div className='row'>
-                        <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'></div>
-                        <div className='col-xs-8 col-sm-8 col-md-8 col-lg-8'>
-                            <div><h1>PARAGRAPH #3</h1></div>
-                            <p>During the Middle Ages, hospitals served different functions from modern institutions. Middle Ages 
-                                hospitals were almshouses for the poor, hostels for pilgrims, or hospital schools. 
-                                The word "hospital" comes from the Latin hospes, signifying a stranger or foreigner, hence a guest. 
-                                Another noun derived from this, hospitium came to signify hospitality, that is the relation between 
-                                guest and shelterer, hospitality, friendliness, and hospitable reception.</p>
-                        </div>
-                        <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'></div>  
-                    </div>
-                    <div className='row'>
-                        <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'></div>
-                        <div className='col-xs-8 col-sm-8 col-md-8 col-lg-8'>
-                            <div><h1>PARAGRAPH #4</h1></div>
-                            <p>During the Middle Ages, hospitals served different functions from modern institutions. Middle Ages 
-                                hospitals were almshouses for the poor, hostels for pilgrims, or hospital schools. 
-                                The word "hospital" comes from the Latin hospes, signifying a stranger or foreigner, hence a guest. 
-                                Another noun derived from this, hospitium came to signify hospitality, that is the relation between 
-                                guest and shelterer, hospitality, friendliness, and hospitable reception.</p>
-                        </div>
-                        <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'></div>  
-                    </div>
-                </Carousel> */}
+import React from 'react';
+import './demo.css'
+import { Slide } from 'react-slideshow-image';
 
 
+const slideImages = [
+'images/card1.jpg',
+'images/card2.jpg',
+'images/card3.jpg'
+];
 
-
-
-
-
-
-
-                <div className='row'>
-                    <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'></div>
-                    <div className='col-xs-8 col-sm-8 col-md-8 col-lg-8'>
-                        <div><h1>PARAGRAPH #1</h1></div>
-                        <p>During the Middle Ages, hospitals served different functions from modern institutions. Middle Ages 
-                            hospitals were almshouses for the poor, hostels for pilgrims, or hospital schools. 
-                            The word "hospital" comes from the Latin hospes, signifying a stranger or foreigner, hence a guest. 
-                            Another noun derived from this, hospitium came to signify hospitality, that is the relation between 
-                            guest and shelterer, hospitality, friendliness, and hospitable reception.</p>
-                    </div>
-                    <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'></div>  
-                </div>
-                <div className='row'>
-                    <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'></div>
-                    <div className='col-xs-8 col-sm-8 col-md-8 col-lg-8'>
-                        <div><h1>PARAGRAPH #2</h1></div>
-                        <p>During the Middle Ages, hospitals served different functions from modern institutions. Middle Ages 
-                            hospitals were almshouses for the poor, hostels for pilgrims, or hospital schools. 
-                            The word "hospital" comes from the Latin hospes, signifying a stranger or foreigner, hence a guest. 
-                            Another noun derived from this, hospitium came to signify hospitality, that is the relation between 
-                            guest and shelterer, hospitality, friendliness, and hospitable reception.</p>
-                    </div> 
-                    <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'></div>  
-                </div>
-                <div className='row'>
-                    <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'></div>
-                    <div className='col-xs-8 col-sm-8 col-md-8 col-lg-8'>
-                        <div><h1>PARAGRAPH #3</h1></div>
-                        <p>During the Middle Ages, hospitals served different functions from modern institutions. Middle Ages 
-                            hospitals were almshouses for the poor, hostels for pilgrims, or hospital schools. 
-                            The word "hospital" comes from the Latin hospes, signifying a stranger or foreigner, hence a guest. 
-                            Another noun derived from this, hospitium came to signify hospitality, that is the relation between 
-                            guest and shelterer, hospitality, friendliness, and hospitable reception.</p>
-                    </div> 
-                    <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'></div>  
-                </div>
-                <div className='row'>
-                    <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'></div>
-                    <div className='col-xs-8 col-sm-8 col-md-8 col-lg-8'>
-                        <div><h1>PARAGRAPH #4</h1></div>
-                        <p>During the Middle Ages, hospitals served different functions from modern institutions. Middle Ages 
-                            hospitals were almshouses for the poor, hostels for pilgrims, or hospital schools. 
-                            The word "hospital" comes from the Latin hospes, signifying a stranger or foreigner, hence a guest. 
-                            Another noun derived from this, hospitium came to signify hospitality, that is the relation between 
-                            guest and shelterer, hospitality, friendliness, and hospitable reception.</p>
-                    </div> 
-                    <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'></div>  
-                </div>
-
-                <div className='row'>
-                    <div className='col-xs-4 col-sm-4 col-md-4 col-lg-4'></div>
-                    <div className='col-xs-4 col-sm-4 col-md-4 col-lg-4'>
-                        <button className='numbtn'>1</button>
-                        <button className='numbtn'>2</button>
-                        <button className='numbtn'>3</button>
-                        <button className='numbtn'>4</button>
-                    </div>
-                    <div className='col-xs-4 col-sm-4 col-md-4 col-lg-4'></div>
-                </div> 
-            </div>
-        );
-    }
+const properties = {
+duration: 5000,
+transitionDuration: 500,
+infinite: true,
+indicators: true,
+arrows: true
 }
 
-export default Paragraph;
+const Slideshow = () => {
+return (
+<div className='row'>
+<div className='col-xs-3 col-sm-3 col-md-3 col-lg-3'></div>
+<div className='col-xs-6 col-sm-6 col-md-6 col-lg-6'>
+<Slide {...properties}>
+<div className="each-slide">
+<div>
+<div><h1 className='slideparahead'>PARAGRAPH #1</h1></div>
+<p className='slidepara'>During the Middle Ages, hospitals served different functions from modern institutions. Middle Ages 
+hospitals were almshouses for the poor, hostels for pilgrims, or hospital schools. 
+The word "hospital" comes from the Latin hospes, signifying a stranger or foreigner, hence a guest. 
+Another noun derived from this, hospitium came to signify hospitality, that is the relation between 
+guest and shelterer, hospitality, friendliness, and hospitable reception.</p>
+</div>
+</div>
+<div className="each-slide">
+<div>
+<div><h1 className='slideparahead'>PARAGRAPH #2</h1></div>
+<p className='slidepara'>During the Middle Ages, hospitals served different functions from modern institutions. Middle Ages 
+hospitals were almshouses for the poor, hostels for pilgrims, or hospital schools. 
+The word "hospital" comes from the Latin hospes, signifying a stranger or foreigner, hence a guest. 
+Another noun derived from this, hospitium came to signify hospitality, that is the relation between 
+guest and shelterer, hospitality, friendliness, and hospitable reception.</p>
+</div>
+</div>
+<div className="each-slide">
+<div>
+<div><h1 className='slideparahead'>PARAGRAPH #3</h1></div>
+<p className='slidepara'>During the Middle Ages, hospitals served different functions from modern institutions. Middle Ages 
+hospitals were almshouses for the poor, hostels for pilgrims, or hospital schools. 
+The word "hospital" comes from the Latin hospes, signifying a stranger or foreigner, hence a guest. 
+Another noun derived from this, hospitium came to signify hospitality, that is the relation between 
+guest and shelterer, hospitality, friendliness, and hospitable reception.</p>
+</div>
+</div>
+<div className="each-slide">
+<div>
+<div><h1 className='slideparahead'>PARAGRAPH #4</h1></div>
+<p className='slidepara'>During the Middle Ages, hospitals served different functions from modern institutions. Middle Ages 
+hospitals were almshouses for the poor, hostels for pilgrims, or hospital schools. 
+The word "hospital" comes from the Latin hospes, signifying a stranger or foreigner, hence a guest. 
+Another noun derived from this, hospitium came to signify hospitality, that is the relation between 
+guest and shelterer, hospitality, friendliness, and hospitable reception.</p>
+</div>
+</div>
+</Slide>
+</div>
+<div className='col-xs-3 col-sm-3 col-md-3 col-lg-3'></div>
+</div>
+)
+}
+export default Slideshow;
