5c3546b42356d72fb7c7c37bca34a0a6a2f07c27
kodeerkarthik <kkrishnegowdaotb1@gmail.com>
2019-07-31 22:55:55 +0530

    commited

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -33,26 +33,29 @@ class Counter extends Component {
                 <div className='row counter'>
                     <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2 eachcounter'>
                         <img src={day} alt={"day"}></img><p>Days a year</p>
-                        <h2 className='countnum'><CountUp  end={365} delay={1} //suffix=' Days'
-                        /></h2>
-                        {this.state.count.map(cont => <span>{cont.days}</span>)}
+                        <h2 className='countnum'>{this.state.count.map(cont => <CountUp  end={cont.days} delay={1} //suffix=' Days'
+                        />)}</h2>
+                        {/* {this.state.count.map(cont => <span>{cont.days}</span>)} */}
                     </div>
                     <div className='col-xs-1 col-sm-1 col-md-1 col-lg-1'></div>
                     <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2 eachcounter'>
                         <img src={patient} alt={"patient"}></img><p>Patient a year</p>
 
-                        <h2 className='countnum'><CountUp  end={564} delay={1}/></h2>
+                        <h2 className='countnum'>{this.state.count.map(cont => <CountUp  end={cont.patients} delay={1} //suffix=' Days'
+                        />)}</h2>
 
                     </div>
                     <div className='col-xs-1 col-sm-1 col-md-1 col-lg-1'></div>
                     <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2 eachcounter'>
                         <img src={doct} alt={"doct"}></img><p>Amazing_Doctors</p>
-                        <h2 className='countnum'><CountUp  end={164} delay={1}/></h2>
+                        <h2 className='countnum'>{this.state.count.map(cont => <CountUp  end={cont.doctors} delay={1} //suffix=' Days'
+                        />)}</h2>
                     </div>
                     <div className='col-xs-1 col-sm-1 col-md-1 col-lg-1'></div>
                     <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2 eachcounter'>
                         <img src={lab} alt={"lab"}></img><p>Lab results</p>
-                        <h2 className='countnum'><CountUp  end={765} delay={1}/></h2>
+                        <h2 className='countnum'>{this.state.count.map(cont => <CountUp  end={cont.lab} delay={1} //suffix=' Days'
+                        />)}</h2>
                     </div>
                     <div className='col-xs-1 col-sm-1 col-md-1 col-lg-1'></div>
                 </div>
