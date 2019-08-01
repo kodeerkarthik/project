a9fcabe6af6ef82da9de85f5a97d63056ef02ee6
kodeerkarthik <kkrishnegowdaotb1@gmail.com>
2019-08-01 22:28:06 +0530

    100% completed

--- src/Components/News.js
+++ src/Components/News.js
@@ -56,6 +56,23 @@ class News extends Component {
         })
     }
 
+    myFunction() {
+        var input, filter, ul, li, a, i, txtValue;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("myUL");
+        li = ul.getElementsByTagName("li");
+        for (i = 0; i < li.length; i++) {
+            a = li[i].getElementsByTagName("h2")[0];
+            txtValue = a.textContent || a.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    }
+
     render() {
         return (
             <div className='newsbody'>
@@ -73,58 +90,63 @@ class News extends Component {
                         <div className='col-xs-8 col-sm-8 col-md-8 col-lg-8'></div>
                         <div className='col-xs-4 col-sm-4 col-md-4 col-lg-4'>
                             <div className='newssearch'>
-                                <input type='text' className='newssearchbox' placeholder='Type here to search latest news'></input>
+                                <input type='text' className='newssearchbox' onChange={this.myFunction} id="myInput" placeholder='Type here to search latest news'></input>
                                 <button className='newssearchbtn'>Search</button>
                             </div>
                         </div>
                     </div>
 
                     <div className='row'>
-                    <div className='col-xs-1 col-sm-1 col-md-1 col-lg-1'></div> 
-                    <div className='col-xs-10 col-sm-10 col-md-10 col-lg-10 row all_news_div'>
-                        <div className='col-xs-4 col-sm-4 col-md-4 col-lg-4'>
-                            <Card className='newscard'>
-                                <CardImg top width="100%" height='100%' src={newsimg} alt="Card image cap" />
-                            </Card>
-                        </div>
-                        <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'></div> 
-                        <div className='col-xs-6 col-sm-6 col-md-6 col-lg-6'>
-
-                            <div className='row c_body ncard'>
-                                <div className='col-xs-3 col-sm-3 col-md-3 col-lg-3'>
-                                    <img src={news1} alt={"logo"} className='news_imges'></img>
-                                </div>
-                                <div className='col-xs-9 col-sm-9 col-md-9 col-lg-9'>
-                                    <div><h2 className='c_title'>Could artificial intelligence?</h2></div>
-                                    <div className='c_txt'>As it stands, doctors have access to high quality imaging, and ... 
-                                    He explains that "in the case of medical imaging, you're lucky if you have 1,000 ...</div>
-                                </div>
+                        <div className='col-xs-1 col-sm-1 col-md-1 col-lg-1'></div> 
+                        <div className='col-xs-10 col-sm-10 col-md-10 col-lg-10 row all_news_div'>
+                            <div className='col-xs-4 col-sm-4 col-md-4 col-lg-4'>
+                                <Card className='newscard'>
+                                    <CardImg top width="100%" height='100%' src={newsimg} alt="Card image cap" />
+                                </Card>
                             </div>
-
-                            <div className='row c_body ncard'>
-                                <div className='col-xs-3 col-sm-3 col-md-3 col-lg-3'>
-                                    <img src={news2} alt={"logo"} className='news_imges'></img>
-                                </div>
-                                <div className='col-xs-9 col-sm-9 col-md-9 col-lg-9'>
-                                    <div><h2 className='c_title'>Causes a pulse in the stomach?</h2></div>
-                                    <div className='c_txt'>According to the National Institutes of Health (NIH), 1 in 10 people who ... 
-                                    of X-ray images to give a detailed picture of tissues inside the body.</div>
-                                </div>
-                            </div>
-
-                            <div className='row c_body ncard'>
-                                <div className='col-xs-3 col-sm-3 col-md-3 col-lg-3'>
-                                    <img src={news3} alt={"logo"} className='news_imges'></img>
-                                </div>
-                                <div className='col-xs-9 col-sm-9 col-md-9 col-lg-9'>
-                                    <div><h2 className='c_title'>Breakthrough discovery</h2></div>
-                                    <div className='c_txt'>Endoscopic imaging, or using cameras inserted directly inside the ... 
-                                    implanted through a medical procedure or surgery in order to reach the ...</div>
-                                </div>
-                            </div>
-                        </div>       
-                    </div> 
-                    <div className='col-xs-1 col-sm-1 col-md-1 col-lg-1'></div> 
+                            <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'></div> 
+                            <div className='col-xs-6 col-sm-6 col-md-6 col-lg-6'>
+                                <ul id="myUL">
+                                    <li>
+                                        <div className='row c_body ncard'>
+                                            <div className='col-xs-3 col-sm-3 col-md-3 col-lg-3'>
+                                                <img src={news1} alt={"logo"} className='news_imges'></img>
+                                            </div>
+                                            <div className='col-xs-9 col-sm-9 col-md-9 col-lg-9'>
+                                                <div><h2 className='c_title'>Could artificial intelligence?</h2></div>
+                                                <div className='c_txt'>As it stands, doctors have access to high quality imaging, and ... 
+                                                He explains that "in the case of medical imaging, you're lucky if you have 1,000 ...</div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className='row c_body ncard'>
+                                            <div className='col-xs-3 col-sm-3 col-md-3 col-lg-3'>
+                                                <img src={news2} alt={"logo"} className='news_imges'></img>
+                                            </div>
+                                            <div className='col-xs-9 col-sm-9 col-md-9 col-lg-9'>
+                                                <div><h2 className='c_title'>Causes a pulse in the stomach?</h2></div>
+                                                <div className='c_txt'>According to the National Institutes of Health (NIH), 1 in 10 people who ... 
+                                                of X-ray images to give a detailed picture of tissues inside the body.</div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className='row c_body ncard'>
+                                            <div className='col-xs-3 col-sm-3 col-md-3 col-lg-3'>
+                                                <img src={news3} alt={"logo"} className='news_imges'></img>
+                                            </div>
+                                            <div className='col-xs-9 col-sm-9 col-md-9 col-lg-9'>
+                                                <div><h2 className='c_title'>Breakthrough discovery</h2></div>
+                                                <div className='c_txt'>Endoscopic imaging, or using cameras inserted directly inside the ... 
+                                                implanted through a medical procedure or surgery in order to reach the ...</div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>       
+                        </div> 
+                        <div className='col-xs-1 col-sm-1 col-md-1 col-lg-1'></div> 
                     </div>
                     <div className='row all_news_div'>
                         <div className='col-xs-1 col-sm-1 col-md-1 col-lg-1'></div>
